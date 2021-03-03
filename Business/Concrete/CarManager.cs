ffe99a9a35e07955477fe106ebd1dded04b9b3b9
mine yiğiz <mineyigiz@hotmail.com>
2021-03-03 19:59:52 +0300

    8.gün Ödev

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -27,12 +27,13 @@ namespace Business.Concrete
         {
             return _carDal.GetAll(c => c.DailyPrice >0);
         }
-  
+
         //public List<Car> GetCarsByCarName(Car cars,Brand brands)
         //{
         //    return _carDal.GetAll(from Car in cars
         //                          join Brand in brands
         //                          on Car.CarId equals Brand.BrandId
+        //                          where len(BrandName) > 2
         //                          select new CarDto
         //                          {
         //                             CarId = Car.CarId,
